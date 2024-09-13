13399154fb526208440a3496d86471c52d5cb395
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2024-09-13 22:45:35 +0530

    refactor register page design

--- app/register/page.js
+++ app/register/page.js
@@ -7,7 +7,7 @@ export default async function RegisterPage() {
   const session = await auth();
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen -m-10">
       <RegisterForm session={session} />
     </div>
   );
